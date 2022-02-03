f5b156654423f69ac4af296940afa6c42ff08232
emailoflvs <emailoflvs@gmail.com>
2022-02-03 17:24:16 +0200

    change docker

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Support\Facades\DB;
 
-//Route::get('/', 'Controller@index')->name('home');
 
-Route::get('/', function () {
-    $visited=[];
-    $togo = [];
+Auth::routes();
 
-  $visited = DB::select('select * from places where visited = ?', [1]);
-  $togo = DB::select('select * from places where visited = ?', [0]);
+Route::get('/', 'Controller@index');
 
-  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
-});
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('/', function () {
+//    $visited=[];
+//    $togo = [];
+//
+//  $visited = DB::select('select * from places where visited = ?', [1]);
+//  $togo = DB::select('select * from places where visited = ?', [0]);
+//
+//  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
+//});
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
