36c71b83c17a6a271a624551850a465aedb4bea2
emailoflvs <emailoflvs@gmail.com>
2022-02-03 18:41:49 +0200

    add voyager

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,7 @@ Route::get('/', 'Controller@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-//Route::get('/', function () {
-//    $visited=[];
-//    $togo = [];
-//
-//  $visited = DB::select('select * from places where visited = ?', [1]);
-//  $togo = DB::select('select * from places where visited = ?', [0]);
-//
-//  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
-//});
 
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
